24e1011708b32c12b69135a00407ca9ad75b8234
Daniel Bates <danielrbates@gmail.com>
2024-10-21 13:38:42 -0400

    Update QueryProducts.ts

--- src/functions/QueryProducts.ts
+++ src/functions/QueryProducts.ts
@@ -5,13 +5,13 @@ export async function QueryProduct(request: HttpRequest, context: InvocationCont
   context.log(`Http function processed request for url "${request.url}"`);
 
   try {
-    // Extract product id from the request body
+    // Extract SQL query from the request body
     const body = await request.json();
     const bodyJson = JSON.stringify(body);
-    const itemId = JSON.parse(bodyJson).id;
+    const userQuery = JSON.parse(bodyJson).query;
 
     // Filter the response object to only product properties
-    let productResponse = await productService.query(itemId);
+    let productResponse = await productService.query(userQuery);
     let productJSON = JSON.stringify(productResponse);
     let product = JSON.parse(productJSON).resources[0];
 
