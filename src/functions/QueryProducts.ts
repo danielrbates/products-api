8b74622e6c7fa97148f53d1a3fbce84034ccda77
Daniel Bates <danielrbates@gmail.com>
2024-10-21 13:48:52 -0400

    Update QueryProducts.ts

--- src/functions/QueryProducts.ts
+++ src/functions/QueryProducts.ts
@@ -1,7 +1,7 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import productService from "../services/product.services";
 
-export async function QueryProduct(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function QueryProducts(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
   try {
