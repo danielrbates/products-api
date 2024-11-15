07574306b84a990e4f28d5a677da9c9e3039eec3
Daniel Bates <danielrbates@gmail.com>
2024-11-15 11:14:14 -0500

    Update GetProducts.ts
    Fixed incorrect function export.

--- src/functions/GetProducts.ts
+++ src/functions/GetProducts.ts
@@ -1,7 +1,7 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import productService from "../services/product.services";
 
-export async function GetAllProducts(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function GetProducts(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
 
   try {
